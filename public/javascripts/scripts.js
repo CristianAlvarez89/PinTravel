62cb4a5b92085f3564d1411645a69e2579ebc439
Rubén Amaro <r.amaro89@gmail.com>
2013-04-29 15:41:51 +0200

    Pestanya de afegir un pin afegida

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -13,3 +13,46 @@ function changeMapType(map)
     }
 }
 
+function openCloseAddCity()
+{
+    if($('#settings-widget').css('display') == 'none')
+    {
+        $('#map_canvas').css('width','75%');
+        $('#settings-widget').show();
+    }
+    else
+    {
+        $('#settings-widget').hide();
+        $('#map_canvas').css('width','100%');
+    }
+}
+function emplenaInformacioLloc(place)
+{
+    $('#cityData').show();
+    var dades = place.formatted_address.split(", ");
+    if(dades.length == 1)
+    {
+        $('#countryName').html(dades[0].trim());
+        $('.co').show();
+        $('.ci').hide();
+        $('.to').hide();
+    }
+    else if(dades.length == 2)
+    {
+        $('#cityName').html(dades[0].trim());
+        $('.ci').show();
+        $('#countryName').html(dades[1].trim());
+        $('.co').show();
+        $('.to').hide();
+    }
+    else
+    {
+        $('#townName').html(dades[0].trim());
+        $('.to').show();
+        $('#cityName').html(dades[1].trim());
+        $('.ci').show();
+        $('#countryName').html(dades[2].trim());
+        $('.co').show();
+    }
+    $('.buttons').show();
+}
